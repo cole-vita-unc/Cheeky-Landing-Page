bb3c70cec06ced04b1d4cd660eb485e2812980ae
Cole <cole.vita@unc.edu>
2023-06-25 21:04:42 -0400

    centered newsletter

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Select all sections
+const sections = document.querySelectorAll('.animated-section');
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to check if any section is in the viewport
+function checkIfInView() {
+    for (const section of sections) {
+        if (isInViewport(section)) {
+            section.classList.add('in-view');
+        } else {
+            section.classList.remove('in-view');
+        }
+    }
+}
+
+// Listen for scroll events
+window.addEventListener('scroll', checkIfInView);
+
+// Check if any section is in the viewport initially
+checkIfInView();
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
